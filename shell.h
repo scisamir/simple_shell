5a1307c7ac8aa737099a6fb57186945a5d88ad14
Samir Idris <samzidris11@gmail.com>
2023-07-26 09:10:43 -0700

    add shell.h

---
+++ shell.h
@@ -0,0 +1,46 @@
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+extern char **environ;
+
+/**
+ * struct builtin - Struct builtin
+ *
+ * @cmnd: a buitin command
+ * @f: a pointer to a function that handles the associated builtin command
+ */
+
+typedef struct builtin
+{
+	char *cmnd;
+	int (*f)(char **args);
+} builtin_t;
+
+
+/* SHELL */
+
+char **parse_input(char *str);
+int check_input(char **args);
+char *find_path(char *cmd);
+int handle_execs(char **args);
+int handle_exit(char **args);
+int handle_cd(char **args);
+void free_words(char **words);
+
+
+/* UTILS */
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+
+#endif /* __SHELL_H__ */
